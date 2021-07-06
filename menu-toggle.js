5a9a80eb0e6c3fdc23b73bda95030d96ff5973b3
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-07-06 14:37:58 -0700

    Adjust js menu-toggler to expand w/ submenu

--- menu-toggle.js
+++ menu-toggle.js
@@ -1,9 +1,22 @@
-function toggleMenu() {
-    var element = document.querySelector(".navbar-left");
-    element.classList.toggle("menuToggle");
-}
+const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+const height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
-function dropMenu() {
-    var element = document.querySelector(".inside-menu");
-    element.classList.toggle("inside-menu-open");
-}
+const mobileMenu = document.getElementsByClassName("burger");
+const nav = document.getElementsByClassName("navbar-left");
+const mobileSub = document.getElementsByClassName("nav-toggle-link");
+const subNav = document.getElementsByClassName("inside-menu");
+
+
+    if(width < 500){
+        mobileMenu.addEventListener('click', function () {
+            nav.classList.toggle("menuToggle");
+        });
+        mobileSub.addEventListener('click', function () {
+        subNav.classList.toggle("subMenuShow");
+        })
+    } else {
+        mobileSub.addEventListener('click', function () {
+            subNav.classList.toggle("subMenuShow");
+        })
+    }
+
