57890bda1f257f0167ff87fdf84d8d2e1058413b
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-07-09 18:52:38 -0700

    Adjust menu toggler

--- menu-toggle.js
+++ menu-toggle.js
@@ -1,22 +1,19 @@
 const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 const height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
-const mobileMenu = document.getElementsByClassName("burger");
+const mobileMenu = document.getElementsByClassName("burger-input");
 const nav = document.getElementsByClassName("navbar-left");
-const mobileSub = document.getElementsByClassName("nav-toggle-link");
+const mobileSub = document.getElementsByClassName("dropdown");
 const subNav = document.getElementsByClassName("inside-menu");
 
 
-    if(width < 500){
-        mobileMenu.addEventListener('click', function () {
-            nav.classList.toggle("menuToggle");
-        });
-        mobileSub.addEventListener('click', function () {
+if(width.value < 500){
+    mobileMenu.addEventListener('click', () => {
+        nav.classList.toggle("menuToggle");
+    });
+    mobileSub.addEventListener('click', () => {
         subNav.classList.toggle("subMenuShow");
-        })
-    } else {
-        mobileSub.addEventListener('click', function () {
-            subNav.classList.toggle("subMenuShow");
-        })
-    }
+    })
+    return;
+}
 
