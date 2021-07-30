74a536d86ee1c888657b45c387b8615a90684f07
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-07-30 10:01:27 -0700

    Debug menu burger toggler

--- menu-toggle.js
+++ menu-toggle.js
@@ -1,12 +1,12 @@
 const width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 const height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
-const mediaQueryMobile = window.matchMedia('(min-width: 320px) and (max-width: 425px)')
+const mediaQueryMobile = window.matchMedia('(max-width: 425px)')
 
-const mobileMenu = document.getElementsByClassName("burger-input");
-const nav = document.getElementsByClassName("navbar-left");
-const mobileSub = document.getElementsByClassName("dropdown");
-const subNav = document.getElementsByClassName("inside-menu");
+const mobileMenu = document.querySelector(".burger-input");
+const nav = document.querySelector(".navbar-left");
+const mobileSub = document.querySelector(".dropdown");
+const subNav = document.querySelector(".inside-menu");
 
 function handleScreenResize(e){
     if(e.matches){
@@ -16,10 +16,7 @@ function handleScreenResize(e){
         mobileSub.addEventListener('click', () => {
             subNav.classList.toggle("subMenuShow");
         });
-    } else {
-        mobileMenu.removeEventListener();
-        subNav.removeEventListener();
-    }
+    } 
 }
 
 mediaQueryMobile.addListener(handleScreenResize)
